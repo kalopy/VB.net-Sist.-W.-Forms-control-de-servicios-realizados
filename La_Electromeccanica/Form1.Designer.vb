e41dd8f0932916e78220e1f7a77edaf28cf10102
kalopy <carlosozuna49@gmail.com>
2022-01-03 18:34:35 -0300

    Agregar archivos de proyecto.

---
+++ La_Electromeccanica/Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gbDC = New System.Windows.Forms.GroupBox()
+        Me.lblCodCliente = New System.Windows.Forms.Label()
+        Me.nudCodCliente = New System.Windows.Forms.NumericUpDown()
+        Me.dtpFecha = New System.Windows.Forms.DateTimePicker()
+        Me.lblFecha = New System.Windows.Forms.Label()
+        Me.lblRuc = New System.Windows.Forms.Label()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblR = New System.Windows.Forms.Label()
+        Me.lblNom = New System.Windows.Forms.Label()
+        Me.gbGrande = New System.Windows.Forms.GroupBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblTot = New System.Windows.Forms.Label()
+        Me.gbServicios = New System.Windows.Forms.GroupBox()
+        Me.lstServicios = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dgvServicioCliente = New System.Windows.Forms.DataGridView()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.dvwServicios = New System.Data.DataView()
+        Me.gbDC.SuspendLayout()
+        CType(Me.nudCodCliente, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbGrande.SuspendLayout()
+        Me.gbServicios.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvServicioCliente, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dvwServicios, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'gbDC
+        '
+        Me.gbDC.Controls.Add(Me.lblCodCliente)
+        Me.gbDC.Controls.Add(Me.nudCodCliente)
+        Me.gbDC.Controls.Add(Me.dtpFecha)
+        Me.gbDC.Controls.Add(Me.lblFecha)
+        Me.gbDC.Controls.Add(Me.lblRuc)
+        Me.gbDC.Controls.Add(Me.lblNombre)
+        Me.gbDC.Controls.Add(Me.lblR)
+        Me.gbDC.Controls.Add(Me.lblNom)
+        Me.gbDC.Location = New System.Drawing.Point(12, 12)
+        Me.gbDC.Name = "gbDC"
+        Me.gbDC.Size = New System.Drawing.Size(776, 100)
+        Me.gbDC.TabIndex = 0
+        Me.gbDC.TabStop = False
+        Me.gbDC.Text = "Datos del cliente"
+        '
+        'lblCodCliente
+        '
+        Me.lblCodCliente.AutoSize = True
+        Me.lblCodCliente.Location = New System.Drawing.Point(6, 32)
+        Me.lblCodCliente.Name = "lblCodCliente"
+        Me.lblCodCliente.Size = New System.Drawing.Size(64, 13)
+        Me.lblCodCliente.TabIndex = 9
+        Me.lblCodCliente.Text = "Cod. Cliente"
+        '
+        'nudCodCliente
+        '
+        Me.nudCodCliente.Location = New System.Drawing.Point(76, 30)
+        Me.nudCodCliente.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.nudCodCliente.Minimum = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.nudCodCliente.Name = "nudCodCliente"
+        Me.nudCodCliente.Size = New System.Drawing.Size(120, 20)
+        Me.nudCodCliente.TabIndex = 8
+        Me.nudCodCliente.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'dtpFecha
+        '
+        Me.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFecha.Location = New System.Drawing.Point(473, 61)
+        Me.dtpFecha.Name = "dtpFecha"
+        Me.dtpFecha.Size = New System.Drawing.Size(96, 20)
+        Me.dtpFecha.TabIndex = 7
+        '
+        'lblFecha
+        '
+        Me.lblFecha.AutoSize = True
+        Me.lblFecha.Location = New System.Drawing.Point(372, 67)
+        Me.lblFecha.Name = "lblFecha"
+        Me.lblFecha.Size = New System.Drawing.Size(95, 13)
+        Me.lblFecha.TabIndex = 6
+        Me.lblFecha.Text = "Fecha Realizacion"
+        '
+        'lblRuc
+        '
+        Me.lblRuc.AutoSize = True
+        Me.lblRuc.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.lblRuc.Location = New System.Drawing.Point(191, 67)
+        Me.lblRuc.Name = "lblRuc"
+        Me.lblRuc.Size = New System.Drawing.Size(0, 13)
+        Me.lblRuc.TabIndex = 5
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.lblNombre.Location = New System.Drawing.Point(56, 67)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(0, 13)
+        Me.lblNombre.TabIndex = 4
+        '
+        'lblR
+        '
+        Me.lblR.AutoSize = True
+        Me.lblR.Location = New System.Drawing.Point(155, 67)
+        Me.lblR.Name = "lblR"
+        Me.lblR.Size = New System.Drawing.Size(30, 13)
+        Me.lblR.TabIndex = 3
+        Me.lblR.Text = "RUC"
+        '
+        'lblNom
+        '
+        Me.lblNom.AutoSize = True
+        Me.lblNom.Location = New System.Drawing.Point(6, 67)
+        Me.lblNom.Name = "lblNom"
+        Me.lblNom.Size = New System.Drawing.Size(44, 13)
+        Me.lblNom.TabIndex = 2
+        Me.lblNom.Text = "Nombre"
+        '
+        'gbGrande
+        '
+        Me.gbGrande.Controls.Add(Me.lblTotal)
+        Me.gbGrande.Controls.Add(Me.lblTot)
+        Me.gbGrande.Controls.Add(Me.gbServicios)
+        Me.gbGrande.Controls.Add(Me.GroupBox1)
+        Me.gbGrande.Controls.Add(Me.btnAgregar)
+        Me.gbGrande.Location = New System.Drawing.Point(12, 130)
+        Me.gbGrande.Name = "gbGrande"
+        Me.gbGrande.Size = New System.Drawing.Size(776, 271)
+        Me.gbGrande.TabIndex = 1
+        Me.gbGrande.TabStop = False
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.ForeColor = System.Drawing.Color.Green
+        Me.lblTotal.Location = New System.Drawing.Point(469, 241)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(49, 20)
+        Me.lblTotal.TabIndex = 6
+        Me.lblTotal.Text = "Total"
+        '
+        'lblTot
+        '
+        Me.lblTot.AutoSize = True
+        Me.lblTot.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTot.ForeColor = System.Drawing.Color.Green
+        Me.lblTot.Location = New System.Drawing.Point(393, 241)
+        Me.lblTot.Name = "lblTot"
+        Me.lblTot.Size = New System.Drawing.Size(49, 20)
+        Me.lblTot.TabIndex = 5
+        Me.lblTot.Text = "Total"
+        '
+        'gbServicios
+        '
+        Me.gbServicios.Controls.Add(Me.lstServicios)
+        Me.gbServicios.Location = New System.Drawing.Point(6, 35)
+        Me.gbServicios.Name = "gbServicios"
+        Me.gbServicios.Size = New System.Drawing.Size(249, 211)
+        Me.gbServicios.TabIndex = 0
+        Me.gbServicios.TabStop = False
+        Me.gbServicios.Text = "Servicios"
+        '
+        'lstServicios
+        '
+        Me.lstServicios.FormattingEnabled = True
+        Me.lstServicios.Location = New System.Drawing.Point(6, 17)
+        Me.lstServicios.Name = "lstServicios"
+        Me.lstServicios.Size = New System.Drawing.Size(237, 186)
+        Me.lstServicios.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dgvServicioCliente)
+        Me.GroupBox1.Location = New System.Drawing.Point(375, 39)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(373, 199)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Servicios agregados"
+        '
+        'dgvServicioCliente
+        '
+        Me.dgvServicioCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvServicioCliente.Location = New System.Drawing.Point(6, 19)
+        Me.dgvServicioCliente.Name = "dgvServicioCliente"
+        Me.dgvServicioCliente.Size = New System.Drawing.Size(361, 174)
+        Me.dgvServicioCliente.TabIndex = 0
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(270, 108)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(88, 40)
+        Me.btnAgregar.TabIndex = 1
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(700, 407)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(88, 40)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.gbGrande)
+        Me.Controls.Add(Me.gbDC)
+        Me.Name = "Form1"
+        Me.Text = "Taller - ""La electromecanica"""
+        Me.gbDC.ResumeLayout(False)
+        Me.gbDC.PerformLayout()
+        CType(Me.nudCodCliente, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbGrande.ResumeLayout(False)
+        Me.gbGrande.PerformLayout()
+        Me.gbServicios.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.dgvServicioCliente, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dvwServicios, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gbDC As GroupBox
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents dgvServicioCliente As DataGridView
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents dtpFecha As DateTimePicker
+    Friend WithEvents lblFecha As Label
+    Friend WithEvents lblRuc As Label
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblR As Label
+    Friend WithEvents lblNom As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblTot As Label
+    Friend WithEvents gbGrande As GroupBox
+    Friend WithEvents gbServicios As GroupBox
+    Friend WithEvents lstServicios As ListBox
+    Friend WithEvents dvwServicios As DataView
+    Friend WithEvents nudCodCliente As NumericUpDown
+    Friend WithEvents lblCodCliente As Label
+End Class
---
+++ La_Electromeccanica/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+
+    '1. Codifique el evento Load del Formulario :
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        conexion.ConnectionString = "data source = KALO; initial catalog = OT_Electro; 
+                                     user id = kalo; password = kalo"
+        CargarLista()
+        CargarGrilla()
+    End Sub
+
+    '1.1. Cargar los servicios al listbox correspondiente. (5 puntos). 
+    Private Sub CargarLista()
+        Try
+            lstServicios.DataSource = generar_datatabla("select * from Servicio")
+            lstServicios.DisplayMember = "Descripcion"
+            lstServicios.ValueMember = "CodServicio"
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    '2. Codifique el evento Leave del control que contiene el código del cliente 
+    Private Sub nudCodCliente_Leave(sender As Object, e As EventArgs) Handles nudCodCliente.Leave
+        Try
+            Dim tabla As New DataTable
+            '2.1. Verificar si existe el cliente con el código ingresado. (5 puntos). 
+            tabla = generar_datatabla("select * from Cliente where CodCliente = '" + nudCodCliente.Text + "'")
+            '2.2. Si existe, mostrar los datos del cliente en las etiquetas correspondientes. (5 puntos). 
+            If tabla.Rows.Count > 0 Then
+                lblNombre.Text = tabla.Rows(0).Item("NombreApellido")
+                lblRuc.Text = tabla.Rows(0).Item("Ruc")
+                '2.3. Si no existe, mostrar un mensaje al usuario. (5 puntos). 
+            Else
+                lblNombre.Text = " "
+                lblRuc.Text = " "
+                lblTotal.Text = " "
+                MsgBox("El cliente no existe")
+                nudCodCliente.Focus()
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    '3. Codifique el evento Click del botón “Agregar”
+
+    '3.1. Validar que el cliente exista. (5 puntos). 
+    Private Function ValidarClienteExista(ByVal CodCliente As Integer) As Boolean
+        Dim resultado As Boolean = False
+        Try
+            Dim tabla As New DataTable
+            tabla = generar_datatabla("select * from Cliente where CodCliente = '" & CodCliente & "'")
+            If tabla.Rows.Count > 0 Then
+                resultado = True
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return resultado
+    End Function
+
+    '3.2. Validar la seleccion del tipo de servicio. (5 puntos)
+    Private Function ValidarSeleccionTipoServicio() As Boolean
+        If (lstServicios.SelectedIndex.Equals(-1)) Then
+            MessageBox.Show("Debe seleccionar el tipo de servicio")
+            Return False
+        End If
+        Return True
+    End Function
+
+    '3.3. Obtener el costo del servicio seleccionado en el listbox de servicios. (5 puntos). 
+    Private Function RecuperarCosto() As Decimal
+
+        Dim tabla As New DataTable
+        Dim Costo As Decimal
+        Try
+            tabla = generar_datatabla("select Costo from Servicio where Descripcion = '" + lstServicios.GetItemText(lstServicios.SelectedItem) + "'")
+            If tabla.Rows.Count > 0 Then
+                Costo = tabla.Rows(0).Item("Costo")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return Costo
+    End Function
+
+    Private Function RecuperarCodServicio() As Integer
+
+        Dim tabla As New DataTable
+        Dim CodServicio As Integer
+        Try
+            tabla = generar_datatabla("select CodServicio from Servicio where Descripcion = '" + lstServicios.GetItemText(lstServicios.SelectedItem) + "'")
+            If tabla.Rows.Count > 0 Then
+                CodServicio = tabla.Rows(0).Item("CodServicio")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return CodServicio
+    End Function
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        Try
+            If ValidarClienteExista(nudCodCliente.Text) = True And ValidarSeleccionTipoServicio() = True Then
+                Dim codServicio As Integer = RecuperarCodServicio()
+                Dim codCliente As Integer = nudCodCliente.Text
+                Dim costo As Decimal = RecuperarCosto()
+                Dim fecha As Date = dtpFecha.Value
+                '3.4. Insertar el servicio para el cliente. (10 puntos). 
+                EjecutarSQL("insert ServicioCliente values (@1,@2,@3,@4)", codServicio, codCliente, costo, fecha)
+                '3.5. Cargar la grilla de servicios realizados del cliente. (5 puntos) 
+                CargarGrilla()
+                '3.6. Acumular el total del costo en la etiqueta lblTotalCosto. (5 puntos). 
+                lblTotal.Text = CostTotal().ToString
+
+                MsgBox("Servicio insertado")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    '3.5. Cargar la grilla de servicios realizados del cliente. (5 puntos) 
+    Private Sub CargarGrilla()
+        Try
+            dvwServicios.Table = generar_datatabla("select a.Descripcion as Servicio,a.Costo,b.FechaRealizacion 
+                                                    from Servicio a join ServicioCliente b 
+                                                    on a.CodServicio = b.CodServicio")
+            dgvServicioCliente.DataSource = dvwServicios
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    '3.6. Acumular el total del costo en la etiqueta lblTotalCosto. (5 puntos).
+    Private Function CostTotal() As Decimal
+
+        Dim tabla As New DataTable
+        Dim CostoTotal As Decimal
+        Try
+            tabla = generar_datatabla("select sum(Costo) as CostoTotal from ServicioCliente where CodCliente = '" + nudCodCliente.Text + "'")
+            If tabla.Rows.Count > 0 Then
+                CostoTotal = tabla.Rows(0).Item("CostoTotal")
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return CostoTotal
+    End Function
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        If MsgBox("¿Desea salir de la aplicacion?", vbQuestion + vbYesNo, "Pregunta") = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+
+End Class
